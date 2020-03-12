0ccc45ef72eaacbacc58a6bc93e25dca1aa7ce6d
Wende Lu <wendelu1991@gmail.com>
2020-03-12 22:47:48 +0800

    object to dict and json

---
+++ object.py
@@ -0,0 +1,17 @@
+import json
+
+class Foo(object):
+    a = 1
+
+    def __init__(self, b, c):
+        self.b = b
+        self.c = c
+
+
+foo = Foo(2, 3)
+foo_dict = foo.__dict__
+foo_json = json.dumps(foo_dict)
+
+print(foo)
+print(foo_dict)
+print(foo_json)
