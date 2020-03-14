faed3bf64e155fc21b2f8ecf261280ba544bf237
Wende Lu <wendelu1991@gmail.com>
2020-03-14 09:54:01 +0800

    init supervisor

---
+++ supervisor/supervisor.py
@@ -0,0 +1,9 @@
+# Supervisor: A Process Control System
+
+# http://supervisord.org/
+
+# pip install supervisor
+
+# init: echo_supervisord_conf > supervisord/supervisord.conf
+
+# run: supervisord -c supervisord/supervisord.conf
