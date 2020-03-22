00a566178c0da70732506e73ce98a3add7c68ac5
Wende Lu <wendelu1991@gmail.com>
2020-03-22 09:15:44 +0800

    robost lock

--- redis/lock.py
+++ redis/lock.py
@@ -6,12 +6,34 @@ import redis
 r = redis.Redis(host='localhost', port=6379, db=0)
 
 
-r.set('foo', 'bar')
+# r.set('foo', 'bar')
 
-r.get('foo')
+# r.get('foo')
+import time
+
+key = "foo"
+if r.set(key, 1, ex=1000, nx=True):
+    try:
+        print("execute")
+        # time.sleep(1000)
+        # import pdb
+        # pdb.set_trace()
+        raise 'abc'
+    finally:
+        r.delete('foo')
+else:
+    print("not execute")
 
 
-if r.setnx('foo', 1):
-    # do ....
-    r.delete('foo')
+
+import time
+
+key = "foo"
+if r.set(key, 1, ex=1000, nx=True):
+    try:
+        print("execute")
+    finally:
+        r.delete('foo')
+else:
+    print("not execute")
 
