fb33e4b8e74c44bc578e4528c51db283f5dad15e
Wende Lu <wendelu1991@gmail.com>
2020-03-24 08:21:19 +0800

    .

--- redis/lock.py
+++ redis/lock.py
@@ -37,3 +37,17 @@ if r.set(key, 1, ex=1000, nx=True):
 else:
     print("not execute")
 
+
+
+
+
+#-----
+@app.task(queue="auction_start_worker")
+def lane_start_worker(lane_id):
+    key = redis_lane_start_lock_key % (lane_id,)
+    if redis.set(key, 1, ex=10, nx=True):
+        try:
+            bid_runtime_service.start(lane_id)
+        finally:
+            redis.delete(key)
+
