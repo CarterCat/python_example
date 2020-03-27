4b2c5d78b38b2b5ce6e5408622d254edd429cc82
Wende Lu <wendelu1991@gmail.com>
2020-03-27 13:45:52 +0800

    exception

---
+++ exception.py
@@ -0,0 +1,31 @@
+class B(Exception):
+    pass
+
+class C(B):
+    pass
+
+class D(C):
+    pass
+
+for cls in [B, C, D]:
+    try:
+        raise cls()
+    except D:
+        print("D")
+    except C:
+        print("C")
+    except B:
+        print("B")
+
+
+class A:
+    class Aaa(Exception):
+        pass
+    def f(self):
+        raise A.Aaa()
+
+try:
+    a = A()
+    a.f()
+except A.Aaa as e:
+    print("catch")
