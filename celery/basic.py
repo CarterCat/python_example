0ea9bcce419db2081f50e0f418b625a8b50c4bdb
Wende Lu <wendelu1991@gmail.com>
2020-03-29 18:11:13 +0800

    celery

---
+++ celery/basic.py
@@ -0,0 +1,17 @@
+# from celery import Celery
+
+# app = Celery("tasks", broker="amqp://localhost")
+
+# @app
+# def test():
+#     print("hello")
+#     return "foo"
+
+
+# r = test.delay()
+
+# r.status
+
+# r.ready()
+
+# r.get()
