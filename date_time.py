39fc780d46e27d6d192e15a274883ecab240dff5
Wende Lu <wendelu1991@gmail.com>
2020-03-29 18:25:26 +0800

    datetime

---
+++ date_time.py
@@ -0,0 +1,22 @@
+
+## 
+
+from datetime import datetime
+
+dt = datetime.now()
+
+dt = datetime.utcnow()
+
+f"{dt.year}-{dt.month}-{dt.day}"
+
+dt.timestamp()
+
+## 
+
+from datetime import timedelta
+
+## 
+
+import time
+
+time.sleep(5)
