acb6386a29acac81ea0a3b693dae54b5ca467925
Wende Lu <wendelu1991@gmail.com>
2020-03-29 22:41:19 +0800

    arrow

--- date_time.py
+++ date_time.py
@@ -27,3 +27,51 @@ dt + timedelta(days=1, hours=12)
 import time
 
 time.sleep(5)
+
+##
+
+import arrow
+
+t = arrow.now()
+
+arrow.utcnow()
+
+arrow.get("1991-10-25 11:11:11")
+
+arrow.get('2013-05-11T21:23:58.970460+07:00')
+
+
+t.shift(hours=-1)
+
+t.shift(weeks=+2)
+
+t.floor("hour")
+
+t.ceil("hour")
+
+t.timestamp
+
+arrow.get(t.timestamp)
+
+t.year
+t.month
+t.day
+t.hour
+t.minute
+t.second
+
+t.datetime # -> datetime.datetime
+
+t.date() # -> datetime.date
+
+t.time() # -> datetime.time
+
+t.format()
+
+t.format('YYYY-MM-DD')
+
+t.format('YYYY-MM-DD HH:mm:ssZZ')
+
+t.humanize()
+
+t.humanize(locale="zh")
