dd7b5896b0c3034f206279fde38a24c47904436f
Wende Lu <wendelu1991@gmail.com>
2020-03-29 18:11:33 +0800

    fmt

--- collection.py
+++ collection.py
@@ -1,7 +1,8 @@
 ##
 
 from collections import namedtuple
-Point = namedtuple('Point', ['x', 'y'])
+
+Point = namedtuple("Point", ["x", "y"])
 p = Point(1, 2)
 p.x
 p.y
@@ -10,22 +11,25 @@ p
 ##
 
 from collections import defaultdict
-dd = defaultdict(lambda: 'foo')
-dd['x']
+
+dd = defaultdict(lambda: "foo")
+dd["x"]
 dd
 
 
 ##
 
 from collections import Counter
+
 counter = Counter()
-counter['a'] += 1
-counter['b'] += 2
+counter["a"] += 1
+counter["b"] += 2
 counter
 
 ##
 
 from collections import deque
+
 dq = deque([1, 2, 3, 4, 5])
 dq.pop()
 dq.popleft()
@@ -36,11 +40,11 @@ dq
 ##
 
 from collections import ChainMap
-a = {'a': 1, 'b': 2}
-b = {'b': 22, 'c': 33}
-c = {'c': 333, 'd': 444}
-cm = ChainMap(a, b, c)
-cm['c']
-cm['d']
-cm
 
+a = {"a": 1, "b": 2}
+b = {"b": 22, "c": 33}
+c = {"c": 333, "d": 444}
+cm = ChainMap(a, b, c)
+cm["c"]
+cm["d"]
+cm
