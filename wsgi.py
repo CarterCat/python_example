bc7a225bed813b414ec34086327f886d0e35f4c9
Wende Lu <wendelu1991@gmail.com>
2020-03-29 22:22:07 +0800

    fmt

--- wsgi.py
+++ wsgi.py
@@ -2,19 +2,18 @@
 
 # app
 
+
 def app(environ, start_response):
-    start_response('200 ok', [('Content-Type', 'text/html')])
-    return [b'<h1>Hello, web!</h1>']
+    start_response("200 ok", [("Content-Type", "text/html")])
+    return [b"<h1>Hello, web!</h1>"]
 
 
 # server
 
 from wsgiref.simple_server import make_server
 
-httpd = make_server('', 8000, app)
+httpd = make_server("", 8000, app)
 
 print("server http on port 8000...")
 
 httpd.serve_forever()
-
-
